86f948ac196d69a15380ad5f4df31f30f6969d3f
like <337698621@qq.com>
2020-07-05 22:22:19 +0800

    react中的css写法

--- src/components/listItem.jsx
+++ src/components/listItem.jsx
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
+import style from './listItem.module.css'
+import classnames from 'classnames/bind';
+import cn from 'classnames';
 
 let count = 0
+const cls = classnames.bind(style);
 class ListItem extends Component {
-
-    constructor( props ) {
-        super(props)
+    constructor(props) {
+        super(props);
         this.state = {}
-    }
+      }
+    
     manageCount() {
         return count + '个'
     }
     render() {
+        const _cn = cn({
+            'themed-grid-col-s': !count
+        })
         return (
             <div className="row mb-3">
-                <div className="col-6 themed-grid-col">{this.props.data.name}</div>
+                <div className="col-6 themed-grid-col">
+                    <span  className={cls('title','list-title')}>
+                        {this.props.data.name}
+                    </span>
+                </div>
                 <div className="col-1 themed-grid-col">￥{this.props.data.price}</div>
-                <div className={`col-2 themed-grid-col${count ? '': '-s'}`}>{count? "":<span>零</span>}</div>
+                {/* <div className={`col-2 themed-grid-col${count ? '' : '-s'}`}>{count ? "" : <span>零</span>}</div> */}
+                <div className={`col-2 themed-grid-col ` + _cn}>{count ? "" : <span>零</span>}</div>
             </div>
         );
     }
--- src/components/listItemFunc.jsx
+++ src/components/listItemFunc.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import style from './listItem.module.css'
+import classnames from 'classnames/bind'
+import cn from 'classnames';
 
 let count = 0;
+const cls = classnames.bind(style);
 
 const ListItem = (props) => {
+    const _cn = cn({
+        'themed-grid-col-s': !count
+    })
     return (
-        <div className="row mb-3">
-            <div className="col-6 theme-grid-col">{props.data.name}</div>
-            <div className="col-1 theme-grid-col">￥{props.data.price}</div>
-            <div className="col-2 theme-grid-col">{count}</div>
+        <div className="row mb-3" style={{ color: '#710000', fontSize: '20' }}>
+            <div className="col-6 themed-grid-col">
+                <span className={cls('title','list-title')}>
+                    {props.data.name}
+                </span>
+            </div>
+            <div className="col-1 themed-grid-col">￥{props.data.price}</div>
+            <div className={`col-2 themed-grid-col ` + _cn}>{count ? "" : <span>零</span>}</div>
         </div>
     );
 }
