07f08a7fb3306d02c543a26dcefe675e70eca251
like <337698621@qq.com>
2020-07-05 20:42:48 +0800

    条件渲染中的三目运算符、函数做条件判断、与运算符&&做条件判断

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import React, { Component } from "react";
-// import ListItem from "./components/listItem";
+import ListItem from "./components/listItem";
 // import ListItem from "./components/listItemFunc";
 
 const listData = [
-  // {
-  //   id: 1,
-  //   name: "sony 65寸高清电视",
-  //   price: 4000,
-  // },
-  // {
-  //   id: 2,
-  //   name: "华为 Meta30",
-  //   price: 6000,
-  // },
-  // {
-  //   id: 3,
-  //   name: "华硕 玩家国度笔记本",
-  //   price: 10000,
-  // },
+  {
+    id: 1,
+    name: "sony 65寸高清电视",
+    price: 4000,
+  },
+  {
+    id: 2,
+    name: "华为 Meta30",
+    price: 6000,
+  },
+  {
+    id: 3,
+    name: "华硕 玩家国度笔记本",
+    price: 10000,
+  },
 ];
 
 class App extends Component {
   renderList() {
-    if(listData.length === 0){
+    if (listData.length === 0) {
       return <div className="text-center">购物车是空的</div>
     }
+    return listData.map(item => {
+      return <ListItem key={item.id} data={item}></ListItem>
+    })
   }
 
   render() {
     return (
       <div className="container">
-        {listData.length === 0 && <div className="text-center">购物车是空的</div>}
+        {/* {listData.length === 0 && <div className="text-center">购物车是空的</div>} */}
         {/* {this.renderList()} */}
+        {listData.map(item => {
+          return <ListItem key={item.id} data={item}></ListItem>
+        })}
       </div>
     );
   }
